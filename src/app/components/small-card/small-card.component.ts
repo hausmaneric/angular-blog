eb19cb4faba64e8b14333e49cbe6ffb9f61c2e0a
hausmaneric <eric.hausman.m@gmail.com>
2022-09-08 18:53:23 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit {
   picCover:string = "";
   @Input()
   cardTitle:string = "";
+  @Input()
+  id:string = "0";
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png",
+    "title":"saiu um novo filme",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, id placeat amet doloremque vitae voluptate cupiditate ullam, consequatur sunt odio, ea nostrum modi quaerat accusamus nemo dolorum corrupti aperiam libero."
+  },
+  {
+    "id":"2",
+    "photoCover":"https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png",
+    "title":"saiu uma nova serie",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, id placeat amet doloremque vitae voluptate cupiditate ullam, consequatur sunt odio, ea nostrum modi quaerat accusamus nemo dolorum corrupti aperiam libero."
+  },
+  {
+    "id":"3",
+    "photoCover":"https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png",
+    "title":"Saiu um velho filme",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, id placeat amet doloremque vitae voluptate cupiditate ullam, consequatur sunt odio, ea nostrum modi quaerat accusamus nemo dolorum corrupti aperiam libero."
+  },
+  {
+    "id":"4",
+    "photoCover":"https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png",
+    "title":"X-men",
+    "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, id placeat amet doloremque vitae voluptate cupiditate ullam, consequatur sunt odio, ea nostrum modi quaerat accusamus nemo dolorum corrupti aperiam libero."
+  }
+]
